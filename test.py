d841a36c73a73fbad956a149d44cab39b53bf3c8
pappachenlipin <pappachenlipin@gmail.com>
2025-10-24 17:13:19 -0400

    Executable file updated

--- test.py
+++ test.py
@@ -1,15 +1,36 @@
-from src.intentclassification.components.data_ingestion import Data_IngestionProcess
+from src.intentclassification.components.data_ingestion import DataIngestionProcess
+from src.intentclassification.components.data_enrichment import DataEnrichmentProcess
+from src.intentclassification.components.data_enrichment import DataPreProcess
 from src.intentclassification.config.config import config
 from src.intentclassification.logging import logging
-
+import pickle
 
 logging.info("Starting the process")
-print("starting the process")
+
 config_obj = config()
 print(f"config the {config_obj}")
-data_ingestion = Data_IngestionProcess(config_obj.get_data_ingestion_config())
+print("starting the Ingestion process")
+data_ingestion = DataIngestionProcess(config_obj.get_data_ingestion_config())
 print(f"data ingestion config {data_ingestion}")
 data_ingestion.ingest_data()
+print("Ending the Ingestion process")
+print("starting the Enrich process")
+data_enrich = DataEnrichmentProcess(config_obj.get_data_ingestion_config(),config_obj.get_data_enrichment_config())
+labelmapping = data_enrich.enrich_data()
+labels_file = os.path.join(config_obj.artifacts_root,"labelmapping.pkl")
+with open(labels_file, "wb") as f:
+    pickle.dump(labelmapping, f)
+print("Ending the Enrich process")
+print("Starting the preprocessing")
+data_preprocess = DataPreProcess(config_obj.get_data_enrichment_config(),config_obj.get_preprocess_config())
+tokenized_train, tokenized_test = data_preprocess.preprocessdata()
+print("Preprocess Completed")
+print("Load the Model")
+
+print("Model Load completed")
+
+
+
 
 
 
